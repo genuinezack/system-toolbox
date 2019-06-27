bce3e48eb6f629a339d508729cadb4f56b63aae9
Zack Sheppard <zack@zacksheppard.com>
2019-06-27 10:11:21 -0400

    Add my zsh tweaks as an oh-my-zsh plugin

---
+++ zsh/zack/README.md
@@ -0,0 +1,11 @@
+# Zack's ZSH Plugins
+
+This plugin is a collection of Zack's most commonly needed terminal lifehacks, wrapped up as a ZSH plugin.
+
+To start using it, add the `zack` plugin to your `plugins` array in `~/.zshrc`:
+
+```zsh
+plugins=(... zack)
+```
+
+To make the most of this plugin, you should be on Mac OS and have the github `hub` command installed.
---
+++ zsh/zack/zack.plugin.zsh
@@ -0,0 +1,32 @@
+# Zack's zsh plugin
+# author: https://github.com/genuinezack
+
+# Shared-shell (bash or zsh or...) terminal tweaks and productivity commands
+
+# Up a directory
+alias up="cd .."
+
+# Set up better alias commands for ls/ll
+alias ls='ls -G'
+alias ll='ls -hl'
+
+# Git "frequently used commands"
+alias grim="git rebase master"
+alias gs="git status"
+alias gg="git grep"
+
+# Pretty formatting compact git log from @jorgeo
+alias glow="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
+# Push current branch to origin without git's "set upstream!" annoyances
+alias gpb="git rev-parse --abbrev-ref HEAD | xargs -n 1 git push origin"
+
+# Push current branch to origin and open a pull request (requires github's hub command)
+alias gipper="gpb && open \`hub pull-request\`"
+
+# CD and immediately LS a directory
+function cdl { cd $1; ls; }
+
+# Define UTF-8 as our terminal language to ensure emoji support!
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
